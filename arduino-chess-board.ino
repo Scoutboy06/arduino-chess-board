bacab531ee421ada305c953fd1260beb9a61922d
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-02-27 15:18:28 +0100

    Update delay

--- arduino-chess-board.ino
+++ arduino-chess-board.ino
@@ -9,7 +9,6 @@ constexpr int lowerThresh = 400;
 
 Multiplexer colMux({A2, A1, A0});
 Multiplexer rowMux({A3, A5, 4});
-// Multiplexer sigMux({2, 3, 4}, IN, A7);
 
 BoardState boardState;
 uint8_t packedData[16] = {0};
@@ -29,16 +28,14 @@ void loop() {
       SquareState s = ReadSquare(row, col);
       boardState.SetSquareState(7 - row, col, s);
     }
-    // PRINT('\n');
   }
-  // PRINT('\n');
 
   boardState.PackTransmitData(packedData);
-  // boardState.DebugPrint();
+
   Serial.write(packedData, sizeof(packedData)); // Send all 16 bytes
   Serial.write('\r\n');                         // Add a newline delimiter
 
-  delay(1000);
+  delay(200);
 }
 
 SquareState ReadSquare(uint8_t rowChannel, uint8_t colChannel) {
@@ -46,8 +43,6 @@ SquareState ReadSquare(uint8_t rowChannel, uint8_t colChannel) {
   colMux.SetChannel(colChannel);
 
   int val = analogRead(A7);
-  // PRINT(val);
-  // PRINT(' ');
 
   if (val < 430)
     return SquareState::Black;
